43a64342156f8b8e1209779725c448f800f38302
Magnus Jahnen <jahnen@in.tum.de>
2016-09-25 15:05:41 -0700

    Exercise 2 gradient descent reg

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -18,9 +18,10 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
-
-
+J = 1 / m * sum(-y' * log(sigmoid(X * theta)) - (1 - y)' * log(1 - sigmoid(X * theta))) ...
+    + lambda / (2 * m) * sum(theta(2:end) .^ 2);
+grad(1) = 1 / m * (sigmoid(X * theta) - y)' * X(:,1);
+grad(2:end) = 1 / m * (sigmoid(X * theta) - y)' * X(:,2:end) + lambda * theta(2:end)' / m;
 
 % =============================================================
 
