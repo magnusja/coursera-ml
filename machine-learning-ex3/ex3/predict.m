04066700c985d08b04ef0edc482fd038d8f22d2f
Magnus Jahnen <jahnen@in.tum.de>
2016-09-26 21:39:45 -0700

    Neural network forward propagation

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,10 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+a_1 = [ones(m, 1) X];
+a_2 = sigmoid(a_1 * Theta1');
+a_2 = [ones(m, 1) a_2];
+[~,p] = max(sigmoid(a_2 * Theta2'), [], 2);
 
 % =========================================================================
 
